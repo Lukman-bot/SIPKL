34d32e6d9c04895d93ad0b99da06118711682f4b
Lukman Aditiya <82071061+Lukman-bot@users.noreply.github.com>
2023-11-17 10:54:34 +0700

    Fixed Bug Pada Halaman Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>AdminLTE 3 | Log in (v2)</title>
+<title>Sistem Informasi PKL | Log in</title>
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 
@@ -44,7 +44,7 @@
                     </div>
                     <div class="row">
                         <div class="col-8">
-                            
+
                         </div>
 
                         <div class="col-4">
@@ -52,7 +52,7 @@
                         </div>
                     </div>
                 </form>
-                
+
             </div>
         </div>
     </div>
