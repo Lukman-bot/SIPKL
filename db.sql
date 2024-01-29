27f403df0a75590a6109bc86421489ab7aa73d4d
Lukman Aditiya <82071061+Lukman-bot@users.noreply.github.com>
2024-01-29 16:24:53 +0700

    Update DB for Siswa

--- db.sql
+++ db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1deb5ubuntu1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Dec 26, 2023 at 01:57 PM
--- Server version: 8.0.35-0ubuntu0.22.04.1
--- PHP Version: 8.1.2-1ubuntu2.14
+-- Host: 127.0.0.1
+-- Generation Time: Jan 29, 2024 at 10:24 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,34 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `dudi`
+-- Table structure for table `detail_siswa`
 --
 
-CREATE TABLE `dudi` (
-  `id_dudi` bigint UNSIGNED NOT NULL,
-  `nama_dudi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `alamat` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `no_telp` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `website` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nama_pimpinan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
+CREATE TABLE `detail_siswa` (
+  `id_detail_siswa` int(11) NOT NULL,
+  `id_pengguna` int(11) NOT NULL,
+  `nis` varchar(50) NOT NULL,
+  `ttl` text NOT NULL,
+  `nama_orang_tua_wali` varchar(250) NOT NULL,
+  `alamat_orang_tua_wali` text NOT NULL,
+  `telp_orang_tua_wali` varchar(15) NOT NULL,
+  `riwayat_kesehatan` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Table structure for table `failed_jobs`
+-- Dumping data for table `detail_siswa`
 --
 
-CREATE TABLE `failed_jobs` (
-  `id` bigint UNSIGNED NOT NULL,
-  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+INSERT INTO `detail_siswa` (`id_detail_siswa`, `id_pengguna`, `nis`, `ttl`, `nama_orang_tua_wali`, `alamat_orang_tua_wali`, `telp_orang_tua_wali`, `riwayat_kesehatan`) VALUES
+(3, 9, '123321', 'Bekasi', 'Lukman', 'Banjar', '089510396303', 'Sehat');
 
 -- --------------------------------------------------------
 
@@ -60,9 +52,9 @@ CREATE TABLE `failed_jobs` (
 --
 
 CREATE TABLE `jenis_pengguna` (
-  `id_jenis_pengguna` bigint UNSIGNED NOT NULL,
-  `jenis_pengguna` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `keterangan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+  `id_jenis_pengguna` char(2) NOT NULL,
+  `jenis_pengguna` varchar(255) NOT NULL,
+  `keterangan` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -70,88 +62,8 @@ CREATE TABLE `jenis_pengguna` (
 --
 
 INSERT INTO `jenis_pengguna` (`id_jenis_pengguna`, `jenis_pengguna`, `keterangan`) VALUES
-(1, 'admin', 'Ini role Admin');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `migrations`
---
-
-CREATE TABLE `migrations` (
-  `id` int UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `migrations`
---
-
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(5, '2023_11_08_043708_create_sekolah_table', 1),
-(6, '2023_11_09_032311_create_jenis_pengguna_table', 1),
-(7, '2023_11_09_032434_create_pengguna_table', 1),
-(8, '2023_11_09_053256_create_dudi_table', 2),
-(9, '2023_11_09_053355_create_pembimbing_dudi_table', 2),
-(10, '2023_11_09_053444_create_penempatan_table', 2),
-(11, '2023_11_09_053530_create_orang_tua_table', 2);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orang_tua`
---
-
-CREATE TABLE `orang_tua` (
-  `id_orang_tua` bigint UNSIGNED NOT NULL,
-  `id_pengguna` bigint UNSIGNED NOT NULL,
-  `nama_orang_tua` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `alamat_orang_tua` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `telp_orang_tua` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `riwayat_kesehatan` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `password_reset_tokens`
---
-
-CREATE TABLE `password_reset_tokens` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pembimbing_dudi`
---
-
-CREATE TABLE `pembimbing_dudi` (
-  `id_pembimbing_dudi` bigint UNSIGNED NOT NULL,
-  `id_dudi` bigint UNSIGNED NOT NULL,
-  `id_pengguna` bigint UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `penempatan`
---
-
-CREATE TABLE `penempatan` (
-  `id_penempatan` bigint UNSIGNED NOT NULL,
-  `id_mitra` bigint UNSIGNED NOT NULL,
-  `tgl_mulai_pkl` date NOT NULL,
-  `tgl_selesai_pkl` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+('11', 'admin', 'Ini role Admin'),
+('21', 'siswa', 'Ini role siswa');
 
 -- --------------------------------------------------------
 
@@ -160,18 +72,18 @@ CREATE TABLE `penempatan` (
 --
 
 CREATE TABLE `pengguna` (
-  `id_pengguna` bigint UNSIGNED NOT NULL,
-  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `kata_kunci` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `gelar_depan` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `nama_lengkap` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `gelar_belakang` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `jenis_kelamin` enum('laki-laki','perempuan') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `golongan_darah` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `alamat` text COLLATE utf8mb4_unicode_ci,
-  `catatan_kesehatan` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `foto_profile` text COLLATE utf8mb4_unicode_ci,
-  `id_jenis_pengguna` bigint UNSIGNED NOT NULL
+  `id_pengguna` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `kata_kunci` text NOT NULL,
+  `gelar_depan` varchar(255) DEFAULT NULL,
+  `nama_lengkap` varchar(255) NOT NULL,
+  `gelar_belakang` varchar(255) DEFAULT NULL,
+  `jenis_kelamin` enum('laki-laki','perempuan') NOT NULL,
+  `golongan_darah` varchar(255) DEFAULT NULL,
+  `alamat` text DEFAULT NULL,
+  `catatan_kesehatan` varchar(255) DEFAULT NULL,
+  `foto_profile` text DEFAULT NULL,
+  `id_jenis_pengguna` char(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -179,75 +91,19 @@ CREATE TABLE `pengguna` (
 --
 
 INSERT INTO `pengguna` (`id_pengguna`, `username`, `kata_kunci`, `gelar_depan`, `nama_lengkap`, `gelar_belakang`, `jenis_kelamin`, `golongan_darah`, `alamat`, `catatan_kesehatan`, `foto_profile`, `id_jenis_pengguna`) VALUES
-(1, 'admin', '$2y$12$7N6Ubn7U/cNl08Y2zGkphO/Ik4NtgS72MVaAyBoVZLpAAn2Wgsi2K', NULL, 'admin', NULL, 'laki-laki', NULL, NULL, NULL, NULL, 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `personal_access_tokens`
---
-
-CREATE TABLE `personal_access_tokens` (
-  `id` bigint UNSIGNED NOT NULL,
-  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tokenable_id` bigint UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `abilities` text COLLATE utf8mb4_unicode_ci,
-  `last_used_at` timestamp NULL DEFAULT NULL,
-  `expires_at` timestamp NULL DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `sekolah`
---
-
-CREATE TABLE `sekolah` (
-  `id_sekolah` bigint UNSIGNED NOT NULL,
-  `nama_sekolah` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `alamat_sekolah` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `telepon_sekolah` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `website` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `alamat_email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` bigint UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+(1, 'admin', '$2y$12$7N6Ubn7U/cNl08Y2zGkphO/Ik4NtgS72MVaAyBoVZLpAAn2Wgsi2K', NULL, 'admin', NULL, 'laki-laki', NULL, NULL, NULL, NULL, '11'),
+(9, '123321', '$2y$12$/.Zl8JyiIAR6xnLfLHkyi.iKh7HwgDRYZw/kDbcjcoksbOuYFSRcq', NULL, 'Lukman', NULL, 'laki-laki', 'A', 'Tasikmalaya', 'Sehat', NULL, '21');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `dudi`
+-- Indexes for table `detail_siswa`
 --
-ALTER TABLE `dudi`
-  ADD PRIMARY KEY (`id_dudi`);
-
---
--- Indexes for table `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+ALTER TABLE `detail_siswa`
+  ADD PRIMARY KEY (`id_detail_siswa`),
+  ADD KEY `fk_detail_siswa_pengguna` (`id_pengguna`);
 
 --
 -- Indexes for table `jenis_pengguna`
@@ -256,165 +112,43 @@ ALTER TABLE `jenis_pengguna`
   ADD PRIMARY KEY (`id_jenis_pengguna`);
 
 --
--- Indexes for table `migrations`
---
-ALTER TABLE `migrations`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `orang_tua`
---
-ALTER TABLE `orang_tua`
-  ADD PRIMARY KEY (`id_orang_tua`),
-  ADD KEY `orang_tua_id_pengguna_foreign` (`id_pengguna`);
-
---
--- Indexes for table `password_reset_tokens`
---
-ALTER TABLE `password_reset_tokens`
-  ADD PRIMARY KEY (`email`);
-
---
--- Indexes for table `pembimbing_dudi`
---
-ALTER TABLE `pembimbing_dudi`
-  ADD PRIMARY KEY (`id_pembimbing_dudi`),
-  ADD KEY `pembimbing_dudi_id_dudi_foreign` (`id_dudi`),
-  ADD KEY `pembimbing_dudi_id_pengguna_foreign` (`id_pengguna`);
-
---
--- Indexes for table `penempatan`
---
-ALTER TABLE `penempatan`
-  ADD PRIMARY KEY (`id_penempatan`),
-  ADD KEY `penempatan_id_mitra_foreign` (`id_mitra`);
-
---
 -- Indexes for table `pengguna`
 --
 ALTER TABLE `pengguna`
   ADD PRIMARY KEY (`id_pengguna`),
-  ADD KEY `pengguna_id_jenis_pengguna_foreign` (`id_jenis_pengguna`);
-
---
--- Indexes for table `personal_access_tokens`
---
-ALTER TABLE `personal_access_tokens`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
-  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
-
---
--- Indexes for table `sekolah`
---
-ALTER TABLE `sekolah`
-  ADD PRIMARY KEY (`id_sekolah`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_email_unique` (`email`);
+  ADD KEY `fk_pengguna_jenis_pengguna` (`id_jenis_pengguna`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `dudi`
+-- AUTO_INCREMENT for table `detail_siswa`
 --
-ALTER TABLE `dudi`
-  MODIFY `id_dudi` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `jenis_pengguna`
---
-ALTER TABLE `jenis_pengguna`
-  MODIFY `id_jenis_pengguna` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `migrations`
---
-ALTER TABLE `migrations`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- AUTO_INCREMENT for table `orang_tua`
---
-ALTER TABLE `orang_tua`
-  MODIFY `id_orang_tua` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `pembimbing_dudi`
---
-ALTER TABLE `pembimbing_dudi`
-  MODIFY `id_pembimbing_dudi` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `penempatan`
---
-ALTER TABLE `penempatan`
-  MODIFY `id_penempatan` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `detail_siswa`
+  MODIFY `id_detail_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `pengguna`
 --
 ALTER TABLE `pengguna`
-  MODIFY `id_pengguna` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `personal_access_tokens`
---
-ALTER TABLE `personal_access_tokens`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `sekolah`
---
-ALTER TABLE `sekolah`
-  MODIFY `id_sekolah` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `orang_tua`
+-- Constraints for table `detail_siswa`
 --
-ALTER TABLE `orang_tua`
-  ADD CONSTRAINT `orang_tua_id_pengguna_foreign` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`id_pengguna`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `pembimbing_dudi`
---
-ALTER TABLE `pembimbing_dudi`
-  ADD CONSTRAINT `pembimbing_dudi_id_dudi_foreign` FOREIGN KEY (`id_dudi`) REFERENCES `dudi` (`id_dudi`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `pembimbing_dudi_id_pengguna_foreign` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`id_pengguna`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `penempatan`
---
-ALTER TABLE `penempatan`
-  ADD CONSTRAINT `penempatan_id_mitra_foreign` FOREIGN KEY (`id_mitra`) REFERENCES `dudi` (`id_dudi`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `detail_siswa`
+  ADD CONSTRAINT `fk_detail_siswa_pengguna` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`id_pengguna`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `pengguna`
 --
 ALTER TABLE `pengguna`
-  ADD CONSTRAINT `pengguna_id_jenis_pengguna_foreign` FOREIGN KEY (`id_jenis_pengguna`) REFERENCES `jenis_pengguna` (`id_jenis_pengguna`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_pengguna_jenis_pengguna` FOREIGN KEY (`id_jenis_pengguna`) REFERENCES `jenis_pengguna` (`id_jenis_pengguna`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
